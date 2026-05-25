ed53bcbffbf5cada1aa0530d32473547cbd38c12
Yassine Aboulhaj <aboulhajyassine@gmail.com>
2026-05-25 04:00:39 +0100

    Addin the missing import to the __init__.py

--- env/__init__.py
+++ env/__init__.py
@@ -1,2 +1,2 @@
-from .cityflow_wrapper import CityFlowEnv
+from .cityflow_wrapper import CityFlowEnv , GREEN_PHASES
 from .graph_builder import build_grid_edge_index, build_graph_from_roadnet, update_graph_features
